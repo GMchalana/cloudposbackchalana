0c7dee9903bcf54d6f267f56f92553237005b7d2
Chalana Prabhashwara <gmchalanaprabhashwara@gmail.com>
2025-08-24 18:27:20 +0530

    fix file path issue

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use('/api/products', require('../routes/products'));
 app.use('/api/orders', require('../routes/orders'));
 app.use('/api/reports', require('../routes/reports'));
 app.use('/api/dashboard', require('../routes/dashboard'));
-app.use('/api/customers', require('./routes/customers'));
+app.use('/api/customers', require('../routes/customers'));
 
 // Error handler
 app.use(errorHandler);
